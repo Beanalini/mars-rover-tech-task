9cbb646e1c2212b5278bd6400ffb3517bac72341
Beanalini <dat826@gmail.com>
2023-11-07 17:00:06 +0000

    test: adds test for size of plateau

--- tests/plateau.test.ts
+++ tests/plateau.test.ts
@@ -1,1 +1,13 @@
-import {} from "../src/plateau";
+import { setPlateauDimensions } from "../src/plateau";
+
+describe("setPlateauDimensions", () => {
+  test("returns the size of the plateau", () => {
+    //Arrange
+    const plateauSizeInput = "5 5";
+    const expectedPlateauSize = { x: 5, y: 5 };
+    //Act
+    const plateauSize = setPlateauDimensions(plateauSizeInput);
+    //Assert
+    expect(plateauSize).toBe(expectedPlateauSize);
+  });
+});
