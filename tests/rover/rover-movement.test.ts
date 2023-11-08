c0d04d24c3caa1de33140f84b231c2e63c8861c6
Beanalini <dat826@gmail.com>
2023-11-08 00:37:10 +0000

    test: for moveRover function

--- tests/rover/rover-movement.test.ts
+++ tests/rover/rover-movement.test.ts
@@ -1,4 +1,5 @@
 import { processMoveInput } from "../../src/rover/rover-move";
+import { processInitialPosition } from "../../src/rover/rover";
 
 const plateauSize = { x: 5, y: 5 };
 describe(" moves rover around the plateau", () => {
@@ -21,5 +22,21 @@ describe(" moves rover around the plateau", () => {
     //Assert
     //returns moveInstructions need to coerce value to a truthy value
     expect(moveInstructions).toBe(false);
+  });
+  test("rover moves forward 1 grid space from inital placement position ", () => {
+    //Arrange
+    const positionInput = "3 3 N";
+    const positionOnPlateau = processInitialPosition(
+      positionInput,
+      plateauSize
+    );
+
+    const moveInput = "M";
+    const moveInstructions = processMoveInput(moveInput);
+    //run movement instruction
+    moveRover(positionOnPlateau, moveInstructions, plateauSize);
+    //Assert
+    //returns RoverPosition need to coerce value to a truthy value
+    expect(positionOnPlateau).toBe({ x: 1, y: 3, direction: "N" });
   });
 });
